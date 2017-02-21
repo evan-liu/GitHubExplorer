cd2de1673ea413c61937196a6e54784c394ebaa5
Evan Liu <evancoding@gmail.com>
2017-02-22 08:05:38 +1300

    Add RequestHeaderSetter plugin to add `Accept` header

--- GitHubExplorer/GitHub/API.swift
+++ GitHubExplorer/GitHub/API.swift
@@ -5,7 +5,11 @@ final class GitHubAPI: StubbableProvider {
     let baseURL = URL(string: "https://api.github.com")!
     let requireAuthentication = true
     var authentication: WebAPIAuthentication?
-    var plugins = PluginHub().addResponseProcessor(ResponseStatusValidator())
+    
+    var plugins = PluginHub()
+        .addRequestProcessor(RequestHeaderSetter(key: .accept, value: "application/vnd.github.v3+json"))
+        .addResponseProcessor(ResponseStatusValidator())
+    
     var httpClient: HTTPClient?
 }
 
